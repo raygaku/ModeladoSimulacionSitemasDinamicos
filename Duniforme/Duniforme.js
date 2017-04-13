15a9989322cefc00fb4ccdc443328f770c4c4fd5
Edgar <hi.ed@hotmail.com>
2017-04-12 23:32:30 -0500

    distribucion uniforme finished

--- Duniforme/Duniforme.js
+++ Duniforme/Duniforme.js
@@ -1,22 +1,34 @@
 angular.module('app',[]).controller('main',['$scope', function($scope){
+  $scope.dataS = {n:'', a:'', b:''};
+  function getRandom(min,max){
+    return parseFloat((Math.random() * (max - min) + min).toFixed(5));
+  }
   $scope.ejecutar = function()
   {
-	  var ng = [0.71857, 0.92784, 0.04921, 0.45197, 0.15191];
-	  var ts = [];
+    console.log($scope.dataS)
+	  var ng = [];
+    for(var i = 0; i < $scope.dataS.n; i++)
+    {
+      ng.push(getRandom(0,1));
+    }
+    var ts = [];
   	var l = ng;
-	  var a = 2;
-	  var b = 4;
-
+	  var a = parseFloat($scope.dataS.a);
+	  var b = parseFloat($scope.dataS.b);
+    
+    $scope.listaNg = ng;
 
 	  for(var i = 0; i < l.length ; i++)
 	  {
 		  operacion = parseFloat( a + (b - a) * l[i]);
-		  ts.push(operacion);
+		  ts.push(parseFloat(operacion.toFixed(5)));
 	  }
+
+    $scope.listaTs = ts;
 	  console.log("Ts");
 	  for(var i = 0; i < ts.length; i++)
 	  {
-		  console.log("ts" + (i+1) + " " + ts[i].toFixed(5));
+		  console.log("ts" + (i+1) + " " + ts[i]);
 	  }
 
 	  var ttoc = 0.0;
@@ -27,11 +39,11 @@ angular.module('app',[]).controller('main',['$scope', function($scope){
 	  });
 
 	  console.log("ttoc ", ttoc.toFixed(5));
-
+    $scope.valorTtoc = ttoc.toFixed(5);
 	  var tpac = 0.0;
 
 	  tpac = parseFloat(ttoc / ts.length);
-
+    $scope.valorTpac = tpac.toFixed(5);
 	  console.log("tpac ", tpac.toFixed(5));
   }
 }]);

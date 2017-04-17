40f7525c184c1a323ccbb372c9d400425d08f821
Edgar <hi.ed@hotmail.com>
2017-04-17 15:17:07 -0500

    working in volados

--- Volados/Volados.js
+++ Volados/Volados.js
@@ -3,4 +3,8 @@ angular.module('app',[]).controller('main',['$scope',function($scope){
   function getRandom(min,max){
     return parseFloat((Math.random() * (max - min) + min).toFixed(5));
   }
+
+  $scope.cantidadInicial = 50;
+  $scope.meta = 80;
+  $scope.apuesta = 10;
 }]);
